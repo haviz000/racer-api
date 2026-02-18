9316968aac9cbac6cc7426ae43aebcae93ba2ee9
Mochamad Haviz Tasmara <mochamad.haviz@dansmultipro.com>
2026-02-18 13:15:51 +0700

    add log to see in railway

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -11,22 +11,39 @@ import (
 )
 
 func LoginController(w http.ResponseWriter, r *http.Request) {
-
+	log.Println("methodnya", r.Method)
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	var req models.LoginRequest
-	json.NewDecoder(r.Body).Decode(&req)
+	if r.Header.Get("Content-Type") != "application/json" {
+		http.Error(w, "Content-Type must be application/json", http.StatusBadRequest)
+		return
+	}
 
-	log.Println("user name controller", req.Username, "kocak", req.Password)
+	var req models.LoginRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		log.Println("decode error:", err)
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+
+	log.Println("user name controller:", req.Username)
+	log.Println("password controller:", req.Password)
+
+	if req.Username == "" || req.Password == "" {
+		http.Error(w, "username and password are required", http.StatusBadRequest)
+		return
+	}
+
 	if err := services.Login(req.Username, req.Password); err != nil {
 		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
 	}
 
 	token, _ := utils.GenerateToken(req.Username)
-	json.NewEncoder(w).Encode(models.LoginResponse{Token: token})
 
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(models.LoginResponse{Token: token})
 }
