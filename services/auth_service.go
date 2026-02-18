5df64a6970ff10a5046bb3658d7b8c452c17bfdc
Mochamad Haviz Tasmara <mochamad.haviz@dansmultipro.com>
2026-02-18 12:59:59 +0700

    add log to see in railway

--- services/auth_service.go
+++ services/auth_service.go
@@ -2,12 +2,14 @@ package services
 
 import (
 	"errors"
+	"log"
 	"os"
 )
 
 func Login(username, password string) error {
 	usernameEnv := os.Getenv("USERNAME_LOGIN_ENV")
 	passwordEnv := os.Getenv("PASSWORD_LOGIN_ENV")
+	log.Println("password", passwordEnv, "kocak", usernameEnv)
 
 	if usernameEnv == "" || passwordEnv == "" {
 		return errors.New("username or password is not set")
