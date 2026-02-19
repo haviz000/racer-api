7341991ce40738d8e4ad0d16fd609ea59401c02f
Mochamad Haviz Tasmara <mochamad.haviz@dansmultipro.com>
2026-02-19 10:39:30 +0700

    update cors

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -6,8 +6,8 @@ func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 		allowed := map[string]bool{
-			"http://localhost:5173":       true,
-			"https://racer-ui.vercel.app": true,
+			"http://localhost:5173":            true,
+			"https://racer-ui-7rxh.vercel.app": true,
 		}
 
 		if allowed[origin] {
